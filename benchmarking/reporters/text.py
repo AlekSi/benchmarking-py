2fc1cbc7b040a4a5ede9ab4f86d09e6603d9acea
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2011-09-01 11:42:06 +0300

    Better text reporter.

--- benchmarking/reporters/text.py
+++ benchmarking/reporters/text.py
@@ -8,24 +8,24 @@ from .reporter import Reporter
 class TextReporter(Reporter):
     """Prints text results to stdout."""
 
-    def __init__(self, min_only=True, name_length=50):
-        self.name_length = name_length
+    def __init__(self, min_only=True):
         self.min_only = min_only
 
     def before_repeat(self, method_name, current, total):
-        print("%d/%d" % (current, total), end='')
+        print("  %5d / %d" % (current, total), end=': ')
+        sys.stdout.flush()
 
     def after_repeat(self, method_name, current, total, calls, result):
         print(result)
 
     def before_benchmark(self, method_name):
-        print(method_name.ljust(self.name_length), ': ', end='')
+        print(method_name, end=':\n')
         sys.stdout.flush()
 
     def after_benchmark(self, method_name, calls, results):
         repeats = len(results)
         if self.min_only:
-            print("%d calls per repeat, best of %d repeats: %f usec per call" % (calls, repeats, self.min_max_time_per_call(calls, results)[0] * 1000000))
+            print("  %d calls per repeat, best of %d repeats: %f usec per call" % (calls, repeats, self.min_max_time_per_call(calls, results)[0] * 1000000))
         else:
-            print("%d calls per repeat, %d repeats: %r usec per call" % (calls, repeats, [time * 1000000 for time in self.times_per_call(calls, results)]))
+            print("  %d calls per repeat, %d repeats: %r usec per call" % (calls, repeats, [time * 1000000 for time in self.times_per_call(calls, results)]))
         sys.stdout.flush()
