a9981550df8873f83a9b0b24a9fad0a56f655e18
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2011-09-05 00:02:51 +0300

    Example with data function.

--- examples/benchmark/benchmark_fibonacci.py
+++ examples/benchmark/benchmark_fibonacci.py
@@ -6,21 +6,24 @@ from examples.fibonacci import FibonacciRecursive, FibonacciRecursiveMemo, Fibon
 
 class FibonacciBenchmarkCase(benchmarking.BenchmarkCase):
     @benchmarking.calls(500)
-    @benchmarking.data(20)
-    def benchmark_recursive(self, n):
-        FibonacciRecursive().get(n)
+    def benchmark_recursive(self):
+        FibonacciRecursive().get(20)
 
     @benchmarking.repeats(3)
-    @benchmarking.data(20)
-    def benchmark_recursive_memo(self, n):
-        FibonacciRecursiveMemo().get(n)
+    def benchmark_recursive_memo(self):
+        FibonacciRecursiveMemo().get(20)
 
     @benchmarking.seconds(1)
     @benchmarking.data(5, 10, 20)
     def benchmark_iterational(self, n):
         FibonacciIterational().get(n)
 
-    @benchmarking.seconds(2)
-    @benchmarking.data(20)
+    def matrix_data():
+        n = 1
+        while n <= 16:
+            yield n
+            n *= 2
+
+    @benchmarking.data_function(matrix_data)
     def benchmark_matrix(self, n):
         FibonacciMatrix().get(n)
