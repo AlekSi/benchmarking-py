aa2bd00effc78bd8b01e0946045c4a3c20a01806
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2011-09-04 21:57:00 +0300

    Better CSV reporter.

--- benchmarking/reporters/csv.py
+++ benchmarking/reporters/csv.py
@@ -12,7 +12,5 @@ class CsvReporter(Reporter):
         import csv
         self.writer = csv.writer(csvfile, dialect=dialect, **fmtparam)
 
-    def after_benchmark(self, method_name, data, calls, results):
-        for n, time in enumerate([time * 1000000 for time in self.times_per_call(calls, results)]):
-            self.writer.writerow([method_name, data, n, time])
-        sys.stdout.flush()
+    def after_repeat(self, method_name, data, current, total, calls, result):
+        self.writer.writerow([method_name, data, current, total, result * 1000000 / calls])
