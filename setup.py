16539ffd44635b207f2948233a215c71f9704875
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2011-09-05 00:04:31 +0300

    Version 0.0.1.

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='benchmarking',
+    version='0.0.1',
+    author='Alexey Palazhchenko',
+    author_email='alexey.palazhchenko@gmail.com',
+    packages=['benchmarking', 'benchmarking/reporters'],
+    url='https://github.com/AlekSi/benchmarking-py',
+    license='MIT',
+    description='Simple benchmark framework (in active development)',
+    long_description=open('README.rst').read(),
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.2",
+        "Topic :: Software Development :: Testing",
+        "Topic :: System :: Benchmark"]
+)
