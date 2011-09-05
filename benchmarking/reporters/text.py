6714f09c55dea133542ba70f7745a216a9cb7375
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2011-09-05 08:04:28 +0400

    Better text reporter.

--- benchmarking/reporters/text.py
+++ benchmarking/reporters/text.py
@@ -15,15 +15,12 @@ repr = r.repr
 class TextReporter(Reporter):
     """Prints text results to stdout."""
 
-    def __init__(self, min_only=True):
-        self.min_only = min_only
-
     def before_repeat(self, method_name, data, current, total):
         print("  %5d / %d" % (current, total), end=': ')
         sys.stdout.flush()
 
     def after_repeat(self, method_name, data, current, total, calls, result):
-        print(result)
+        print('%f usec per call' % (result * 1000000 / calls))
 
     def before_benchmark(self, method_name, data):
         print('%s(%s)' % (method_name, repr(data)), end=':\n')
@@ -31,8 +28,5 @@ class TextReporter(Reporter):
 
     def after_benchmark(self, method_name, data, calls, results):
         repeats = len(results)
-        if self.min_only:
-            print("  %d calls per repeat, best of %d repeats: %f usec per call" % (calls, repeats, self.min_max_time_per_call(calls, results)[0] * 1000000))
-        else:
-            print("  %d calls per repeat, %d repeats: %r usec per call" % (calls, repeats, [time * 1000000 for time in self.times_per_call(calls, results)]))
+        print("  %d calls per repeat, best of %d repeats: %f usec per call" % (calls, repeats, self.min_max_time_per_call(calls, results)[0] * 1000000))
         sys.stdout.flush()
