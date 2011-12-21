52d69ad67273a4eac6210bb706e3884b9142ed9c
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2011-12-21 13:26:01 +0400

    Version 0.0.7.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='benchmarking',
-    version='0.0.6',
+    version='0.0.7',
     author='Alexey Palazhchenko',
     author_email='alexey.palazhchenko@gmail.com',
     packages=['benchmarking', 'benchmarking/reporters'],
